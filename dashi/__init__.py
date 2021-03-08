38ff0acf7c65af106ddf82701ff172e6cc80f070
jgiannelos <jgiannelos@wikimedia.org>
2021-03-08 10:17:03 +0100

    Automatic styling fixes. Remove unused modules.

--- dashi/__init__.py
+++ dashi/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for dashi."""
 
 __author__ = """John Giannelos"""
-__email__ = 'johngiannelos@gmail.com'
-__version__ = '0.1.0'
+__email__ = "johngiannelos@gmail.com"
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -2,52 +2,52 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'kombu']
+requirements = ["Click>=7.0", "kombu"]
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="John Giannelos",
-    author_email='johngiannelos@gmail.com',
-    python_requires='>=3.5',
+    author_email="johngiannelos@gmail.com",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Dashi is a simple message queue CLI tool",
     entry_points={
-        'console_scripts': [
-            'dashi=dashi.cli:main',
+        "console_scripts": [
+            "dashi=dashi.cli:main",
         ],
     },
     install_requires=requirements,
     license="GNU General Public License v3",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='dashi',
-    name='dashi',
-    packages=find_packages(include=['dashi', 'dashi.*']),
+    keywords="dashi",
+    name="dashi",
+    packages=find_packages(include=["dashi", "dashi.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/johngian/dashi',
-    version='0.1.0',
+    url="https://github.com/johngian/dashi",
+    version="0.1.0",
     zip_safe=False,
 )
